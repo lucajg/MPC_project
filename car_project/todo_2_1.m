24811cde2486fab6ae2dbedb4c0bcc39e6a37f51
Geoffroy1312 <geoffroy1312@gmail.com>
2024-12-10 19:23:59 +0100

    jadore luca

--- car_project/todo_2_1.m
+++ car_project/todo_2_1.m
@@ -1,6 +1,7 @@
 car = Car(Ts);
 Vs = 120/3.6;                       % 120 km/h
-[xs, us] = car.steady_state(Vs);    % Compute steady−state for which f s(xs,us) = 0
-sys = car.linearize(xs, us);        % Linearize the nonlinear model around xs, us
+[xs, us] = car.steady_state(Vs)    % Compute steady−state for which f s(xs,us) = 0
+sys = car.linearize(xs, us)        % Linearize the nonlinear model around xs, us
+sys.UserData.f_xs_us
 
 %test_je_te_st
